12892d13f52b59680415b703e13a1be4b683d7ab
Khaza <khazaichowdhury@gmail.com>
2025-12-21 13:33:46 -0800

    404page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Homepage from './pages/Homepage';
-import Photography from './pages/Photography';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Photography from "./pages/Photography";
 // import ComputerScience from './pages/ComputerScience';
 // import AboutMe from './pages/AboutMe';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/khazaichowdhury.com">
       <div className="App">
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/khazaichowdhury.com" element={<Homepage />} />
-            <Route path="/khazaichowdhury.com/photography" element={<Photography />} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="/photography" element={<Photography />} />
             {/* <Route path="/computer-science" element={<ComputerScience />} /> */}
             {/* <Route path="/about" element={<AboutMe />} /> */}
           </Routes>
